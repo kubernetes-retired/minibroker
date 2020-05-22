kubernetes-retired/minibroker

commit 738f50b4ec2266f437b7a869c8eb15d68844f4ac

refactor: small fixes and refactors (#54)
* fix: use _ for unused fn args to satisfy iface

Signed-off-by: Thulio Ferraz Assis <thulio.assis@suse.com>

* fix: remove unused argument from method

Signed-off-by: Thulio Ferraz Assis <thulio.assis@suse.com>

* refactor: move operation key logic

Signed-off-by: Thulio Ferraz Assis <thulio.assis@suse.com>

* fix: use function instead of slice for pointer

Signed-off-by: Thulio Ferraz Assis <thulio.assis@suse.com>

* refactor: improve helm.LoadChart logic

Signed-off-by: Thulio Ferraz Assis <thulio.assis@suse.com>

* refactor: unwrap chartutil.Load function call

Signed-off-by: Thulio Ferraz Assis <thulio.assis@suse.com>

* fix: helm error message

Signed-off-by: Thulio Ferraz Assis <thulio.assis@suse.com>

* refactor: improve minibroker error messages

Signed-off-by: Thulio Ferraz Assis <thulio.assis@suse.com>

* refactor: unwrap updateConfigMap call

Signed-off-by: Thulio Ferraz Assis <thulio.assis@suse.com>

pkg/broker/broker.go
@@ -18,6 +18,7 @@ package broker
 
 import (
 	"errors"
+	"fmt"
 	"sync"
 
 	"github.com/golang/glog"
@@ -75,7 +76,7 @@ func (b *Broker) GetCatalog(c *broker.RequestContext) (*broker.CatalogResponse, 
 	return response, nil
 }
 
-func (b *Broker) Provision(request *osb.ProvisionRequest, c *broker.RequestContext) (*broker.ProvisionResponse, error) {
+func (b *Broker) Provision(request *osb.ProvisionRequest, _ *broker.RequestContext) (*broker.ProvisionResponse, error) {
 	b.Lock()
 	defer b.Unlock()
 
@@ -109,7 +110,7 @@ func (b *Broker) Provision(request *osb.ProvisionRequest, c *broker.RequestConte
 	return &response, nil
 }
 
-func (b *Broker) Deprovision(request *osb.DeprovisionRequest, c *broker.RequestContext) (*broker.DeprovisionResponse, error) {
+func (b *Broker) Deprovision(request *osb.DeprovisionRequest, _ *broker.RequestContext) (*broker.DeprovisionResponse, error) {
 	glog.V(5).Infof("Deprovisioning %s (%s/%s)", request.InstanceID, request.ServiceID, request.PlanID)
 	b.Lock()
 	defer b.Unlock()
@@ -132,12 +133,15 @@ func (b *Broker) Deprovision(request *osb.DeprovisionRequest, c *broker.RequestC
 }
 
 // LastOperation provides information on the state of the last asynchronous operation
-func (b *Broker) LastOperation(request *osb.LastOperationRequest, c *broker.RequestContext) (*broker.LastOperationResponse, error) {
+func (b *Broker) LastOperation(request *osb.LastOperationRequest, _ *broker.RequestContext) (*broker.LastOperationResponse, error) {
 	glog.V(5).Infof("Getting last operation of %s (%v/%v)", request.InstanceID, request.ServiceID, request.PlanID)
 	b.Lock()
 	defer b.Unlock()
 
-	response, err := b.Client.LastOperationState(request.InstanceID, request.OperationKey)
+	if request.OperationKey == nil {
+		return nil, fmt.Errorf("failed to get last operation for instance %q: operation key cannot be nil", request.InstanceID)
+	}
+	response, err := b.Client.LastOperationState(request.InstanceID, *request.OperationKey)
 	if err != nil {
 		glog.Errorln(err)
 		return nil, err
@@ -149,7 +153,7 @@ func (b *Broker) LastOperation(request *osb.LastOperationRequest, c *broker.Requ
 	return &wrappedResponse, nil
 }
 
-func (b *Broker) Bind(request *osb.BindRequest, c *broker.RequestContext) (*broker.BindResponse, error) {
+func (b *Broker) Bind(request *osb.BindRequest, _ *broker.RequestContext) (*broker.BindResponse, error) {
 	glog.V(5).Infof("Binding %s (%s)", request.InstanceID, request.ServiceID)
 	b.Lock()
 	defer b.Unlock()
@@ -174,7 +178,7 @@ func (b *Broker) Bind(request *osb.BindRequest, c *broker.RequestContext) (*brok
 	}
 
 	// Get the response back out of the configmaps
-	operationState, err := b.Client.LastBindingOperationState(request.InstanceID, request.BindingID, &operationKey)
+	operationState, err := b.Client.LastBindingOperationState(request.InstanceID, request.BindingID)
 	if err != nil {
 		glog.Errorln(err)
 		return nil, err
@@ -203,7 +207,7 @@ func (b *Broker) Bind(request *osb.BindRequest, c *broker.RequestContext) (*brok
 	return &bindResponse, nil
 }
 
-func (b *Broker) GetBinding(request *osb.GetBindingRequest, c *broker.RequestContext) (*broker.GetBindingResponse, error) {
+func (b *Broker) GetBinding(request *osb.GetBindingRequest, _ *broker.RequestContext) (*broker.GetBindingResponse, error) {
 	binding, err := b.Client.GetBinding(request.InstanceID, request.BindingID)
 	if err != nil {
 		glog.Errorln(err)
@@ -215,8 +219,8 @@ func (b *Broker) GetBinding(request *osb.GetBindingRequest, c *broker.RequestCon
 	return &response, nil
 }
 
-func (b *Broker) BindingLastOperation(request *osb.BindingLastOperationRequest, c *broker.RequestContext) (*broker.LastOperationResponse, error) {
-	state, err := b.Client.LastBindingOperationState(request.InstanceID, request.BindingID, request.OperationKey)
+func (b *Broker) BindingLastOperation(request *osb.BindingLastOperationRequest, _ *broker.RequestContext) (*broker.LastOperationResponse, error) {
+	state, err := b.Client.LastBindingOperationState(request.InstanceID, request.BindingID)
 	if err != nil {
 		glog.Errorln(err)
 		return nil, err
@@ -242,7 +246,7 @@ func (b *Broker) Unbind(request *osb.UnbindRequest, c *broker.RequestContext) (*
 	return &response, nil
 }
 
-func (b *Broker) Update(request *osb.UpdateInstanceRequest, c *broker.RequestContext) (*broker.UpdateInstanceResponse, error) {
+func (b *Broker) Update(request *osb.UpdateInstanceRequest, _ *broker.RequestContext) (*broker.UpdateInstanceResponse, error) {
 	// Not supported, do nothing
 
 	response := broker.UpdateInstanceResponse{}

pkg/minibroker/minibroker.go
@@ -345,7 +345,7 @@ func (c *Client) Provision(instanceID, serviceID, planID, namespace string, acce
 			OperationDescriptionKey: fmt.Sprintf("provisioning service instance %q", instanceID),
 		})
 		if err != nil {
-			return "", errors.Wrapf(err, "Failed to set operation key when provisioning instance %s", instanceID)
+			return "", errors.Wrapf(err, "Failed to set operation key when provisioning instance %q", instanceID)
 		}
 		go func() {
 			err = c.provisionSynchronously(instanceID, namespace, serviceID, planID, chartName, chartVersion, provisionParams)
@@ -385,13 +385,15 @@ func (c *Client) provisionSynchronously(instanceID, namespace, serviceID, planID
 		return err
 	}
 
+	chartURL := chartDef.URLs[0]
+
 	tc, close, err := c.connectTiller()
 	if err != nil {
 		return err
 	}
 	defer close()
 
-	chart, err := helm.LoadChart(chartDef)
+	chart, err := helm.LoadChart(chartURL)
 	if err != nil {
 		return err
 	}
@@ -659,11 +661,16 @@ func (c *Client) bindSynchronously(instanceID, serviceID, bindingID, releaseName
 
 // Unbind a previously-bound instance binding.
 func (c *Client) Unbind(instanceID, bindingID string) error {
-	// The only clean up we need to do is to remove the binding information
-	return c.updateConfigMap(instanceID, map[string]interface{}{
+	// The only clean up we need to do is to remove the binding information.
+	data := map[string]interface{}{
 		(BindingStateKeyPrefix + bindingID): nil,
 		(BindingKeyPrefix + bindingID):      nil,
-	})
+	}
+	if err := c.updateConfigMap(instanceID, data); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (c *Client) GetBinding(instanceID, bindingID string) (*osb.GetBindingResponse, error) {
@@ -755,7 +762,7 @@ func (c *Client) deprovisionSynchronously(instanceID, release string) error {
 }
 
 // LastOperationState returns the status of the last asynchronous operation.
-func (c *Client) LastOperationState(instanceID string, operationKey *osb.OperationKey) (*osb.LastOperationResponse, error) {
+func (c *Client) LastOperationState(instanceID string, operationKey osb.OperationKey) (*osb.LastOperationResponse, error) {
 	config, err := c.coreClient.CoreV1().ConfigMaps(c.namespace).Get(instanceID, metav1.GetOptions{})
 	if err != nil {
 		if apierrors.IsNotFound(err) {
@@ -768,12 +775,12 @@ func (c *Client) LastOperationState(instanceID string, operationKey *osb.Operati
 		return nil, err
 	}
 
-	if operationKey != nil && config.Data[OperationNameKey] != string(*operationKey) {
-		// Got unexpected operation key
+	if config.Data[OperationNameKey] != string(operationKey) {
+		// Got unexpected operation key.
 		return nil, osb.HTTPStatusCodeError{
 			StatusCode:   http.StatusBadRequest,
-			ErrorMessage: &[]string{ConcurrencyErrorMessage}[0],
-			Description:  &[]string{ConcurrencyErrorDescription}[0],
+			ErrorMessage: strPtr(ConcurrencyErrorMessage),
+			Description:  strPtr(ConcurrencyErrorDescription),
 		}
 	}
 
@@ -792,7 +799,7 @@ func strPtr(value string) *string {
 	return &value
 }
 
-func (c *Client) LastBindingOperationState(instanceID, bindingID string, operationKey *osb.OperationKey) (*osb.LastOperationResponse, error) {
+func (c *Client) LastBindingOperationState(instanceID, bindingID string) (*osb.LastOperationResponse, error) {
 	config, err := c.getConfigMap(instanceID)
 	if err != nil {
 		if apierrors.IsNotFound(err) {

pkg/helm/helm.go
@@ -123,9 +123,7 @@ func (c *Client) GetChart(name, version string) (*repo.ChartVersion, error) {
 	return nil, fmt.Errorf("version not found: %s @ %s", name, version)
 }
 
-func LoadChart(chart *repo.ChartVersion) (*chart.Chart, error) {
-	chartURL := chart.URLs[0]
-
+func LoadChart(chartURL string) (*chart.Chart, error) {
 	glog.Infof("downloading chart from %s", chartURL)
 	resp, err := http.Get(chartURL)
 	if err != nil {
@@ -156,9 +154,14 @@ func LoadChart(chart *repo.ChartVersion) (*chart.Chart, error) {
 
 	glog.Infof("copying chart to %s", fullChartPath)
 	if _, err := io.Copy(fd, resp.Body); err != nil {
-		return nil, errors.Wrapf(err, "failed to copy chart contents to %s", fullChartPath)
+		return nil, errors.Wrapf(err, "failed to download chart contents to %s", fullChartPath)
 	}
 
 	glog.Infof("loading chart from %s on disk", fullChartPath)
-	return chartutil.Load(fullChartPath)
+	chart, err := chartutil.Load(fullChartPath)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to load chart from disk")
+	}
+
+	return chart, nil
 }

commit 797bc20a996bd8d3098b272fd2ad2ffc8b0f25d4

fix: bug introduced by #54
https://github.com/kubernetes-sigs/minibroker/pull/54/commits/3553969776700d7a8a07a81541f4be507403e9f6

Signed-off-by: Thulio Ferraz Assis <thulio.assis@suse.com>

pkg/broker/broker.go
@@ -18,7 +18,6 @@ package broker
 
 import (
 	"errors"
-	"fmt"
 	"sync"
 
 	"github.com/golang/glog"
@@ -138,10 +137,7 @@ func (b *Broker) LastOperation(request *osb.LastOperationRequest, _ *broker.Requ
 	b.Lock()
 	defer b.Unlock()
 
-	if request.OperationKey == nil {
-		return nil, fmt.Errorf("failed to get last operation for instance %q: operation key cannot be nil", request.InstanceID)
-	}
-	response, err := b.Client.LastOperationState(request.InstanceID, *request.OperationKey)
+	response, err := b.Client.LastOperationState(request.InstanceID, request.OperationKey)
 	if err != nil {
 		glog.Errorln(err)
 		return nil, err

pkg/minibroker/minibroker.go
@@ -762,7 +762,7 @@ func (c *Client) deprovisionSynchronously(instanceID, release string) error {
 }
 
 // LastOperationState returns the status of the last asynchronous operation.
-func (c *Client) LastOperationState(instanceID string, operationKey osb.OperationKey) (*osb.LastOperationResponse, error) {
+func (c *Client) LastOperationState(instanceID string, operationKey *osb.OperationKey) (*osb.LastOperationResponse, error) {
 	config, err := c.coreClient.CoreV1().ConfigMaps(c.namespace).Get(instanceID, metav1.GetOptions{})
 	if err != nil {
 		if apierrors.IsNotFound(err) {
@@ -775,7 +775,7 @@ func (c *Client) LastOperationState(instanceID string, operationKey osb.Operatio
 		return nil, err
 	}
 
-	if config.Data[OperationNameKey] != string(operationKey) {
+	if operationKey != nil && config.Data[OperationNameKey] != string(*operationKey) {
 		// Got unexpected operation key.
 		return nil, osb.HTTPStatusCodeError{
 			StatusCode:   http.StatusBadRequest,