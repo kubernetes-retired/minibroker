kubernetes-retired/minibroker

commit 738f50b4ec2266f437b7a869c8eb15d68844f4ac

refactor: small fixes and refactors (#54)
* fix: use _ for unused fn args to satisfy iface

Signed-off-by: Thulio Ferraz Assis <thulio.assis@suse.com>

* fix: remove unused argument from method

Signed-off-by: Thulio Ferraz Assis <thulio.assis@suse.com>

* refactor: move operation key logic

Signed-off-by: Thulio Ferraz Assis <thulio.assis@suse.com>

* fix: use function instead of slice for pointer

Signed-off-by: Thulio Ferraz Assis <thulio.assis@suse.com>

* refactor: improve helm.LoadChart logic

Signed-off-by: Thulio Ferraz Assis <thulio.assis@suse.com>

* refactor: unwrap chartutil.Load function call

Signed-off-by: Thulio Ferraz Assis <thulio.assis@suse.com>

* fix: helm error message

Signed-off-by: Thulio Ferraz Assis <thulio.assis@suse.com>

* refactor: improve minibroker error messages

Signed-off-by: Thulio Ferraz Assis <thulio.assis@suse.com>

* refactor: unwrap updateConfigMap call

Signed-off-by: Thulio Ferraz Assis <thulio.assis@suse.com>

pkg/broker/broker.go
@@ -18,6 +18,7 @@ package broker
 
 import (
 	"errors"
+	"fmt"
 	"sync"
 
 	"github.com/golang/glog"
@@ -75,7 +76,7 @@ func (b *Broker) GetCatalog(c *broker.RequestContext) (*broker.CatalogResponse, 
 	return response, nil
 }
 
-func (b *Broker) Provision(request *osb.ProvisionRequest, c *broker.RequestContext) (*broker.ProvisionResponse, error) {
+func (b *Broker) Provision(request *osb.ProvisionRequest, _ *broker.RequestContext) (*broker.ProvisionResponse, error) {
 	b.Lock()
 	defer b.Unlock()
 
@@ -109,7 +110,7 @@ func (b *Broker) Provision(request *osb.ProvisionRequest, c *broker.RequestConte
 	return &response, nil
 }
 
-func (b *Broker) Deprovision(request *osb.DeprovisionRequest, c *broker.RequestContext) (*broker.DeprovisionResponse, error) {
+func (b *Broker) Deprovision(request *osb.DeprovisionRequest, _ *broker.RequestContext) (*broker.DeprovisionResponse, error) {
 	glog.V(5).Infof("Deprovisioning %s (%s/%s)", request.InstanceID, request.ServiceID, request.PlanID)
 	b.Lock()
 	defer b.Unlock()
@@ -132,12 +133,15 @@ func (b *Broker) Deprovision(request *osb.DeprovisionRequest, c *broker.RequestC
 }
 
 // LastOperation provides information on the state of the last asynchronous operation
-func (b *Broker) LastOperation(request *osb.LastOperationRequest, c *broker.RequestContext) (*broker.LastOperationResponse, error) {
+func (b *Broker) LastOperation(request *osb.LastOperationRequest, _ *broker.RequestContext) (*broker.LastOperationResponse, error) {
 	glog.V(5).Infof("Getting last operation of %s (%v/%v)", request.InstanceID, request.ServiceID, request.PlanID)
 	b.Lock()
 	defer b.Unlock()
 
-	response, err := b.Client.LastOperationState(request.InstanceID, request.OperationKey)
+	if request.OperationKey == nil {
+		return nil, fmt.Errorf("failed to get last operation for instance %q: operation key cannot be nil", request.InstanceID)
+	}
+	response, err := b.Client.LastOperationState(request.InstanceID, *request.OperationKey)
 	if err != nil {
 		glog.Errorln(err)
 		return nil, err
@@ -149,7 +153,7 @@ func (b *Broker) LastOperation(request *osb.LastOperationRequest, c *broker.Requ
 	return &wrappedResponse, nil
 }
 
-func (b *Broker) Bind(request *osb.BindRequest, c *broker.RequestContext) (*broker.BindResponse, error) {
+func (b *Broker) Bind(request *osb.BindRequest, _ *broker.RequestContext) (*broker.BindResponse, error) {
 	glog.V(5).Infof("Binding %s (%s)", request.InstanceID, request.ServiceID)
 	b.Lock()
 	defer b.Unlock()
@@ -174,7 +178,7 @@ func (b *Broker) Bind(request *osb.BindRequest, c *broker.RequestContext) (*brok
 	}
 
 	// Get the response back out of the configmaps
-	operationState, err := b.Client.LastBindingOperationState(request.InstanceID, request.BindingID, &operationKey)
+	operationState, err := b.Client.LastBindingOperationState(request.InstanceID, request.BindingID)
 	if err != nil {
 		glog.Errorln(err)
 		return nil, err
@@ -203,7 +207,7 @@ func (b *Broker) Bind(request *osb.BindRequest, c *broker.RequestContext) (*brok
 	return &bindResponse, nil
 }
 
-func (b *Broker) GetBinding(request *osb.GetBindingRequest, c *broker.RequestContext) (*broker.GetBindingResponse, error) {
+func (b *Broker) GetBinding(request *osb.GetBindingRequest, _ *broker.RequestContext) (*broker.GetBindingResponse, error) {
 	binding, err := b.Client.GetBinding(request.InstanceID, request.BindingID)
 	if err != nil {
 		glog.Errorln(err)
@@ -215,8 +219,8 @@ func (b *Broker) GetBinding(request *osb.GetBindingRequest, c *broker.RequestCon
 	return &response, nil
 }
 
-func (b *Broker) BindingLastOperation(request *osb.BindingLastOperationRequest, c *broker.RequestContext) (*broker.LastOperationResponse, error) {
-	state, err := b.Client.LastBindingOperationState(request.InstanceID, request.BindingID, request.OperationKey)
+func (b *Broker) BindingLastOperation(request *osb.BindingLastOperationRequest, _ *broker.RequestContext) (*broker.LastOperationResponse, error) {
+	state, err := b.Client.LastBindingOperationState(request.InstanceID, request.BindingID)
 	if err != nil {
 		glog.Errorln(err)
 		return nil, err
@@ -242,7 +246,7 @@ func (b *Broker) Unbind(request *osb.UnbindRequest, c *broker.RequestContext) (*
 	return &response, nil
 }
 
-func (b *Broker) Update(request *osb.UpdateInstanceRequest, c *broker.RequestContext) (*broker.UpdateInstanceResponse, error) {
+func (b *Broker) Update(request *osb.UpdateInstanceRequest, _ *broker.RequestContext) (*broker.UpdateInstanceResponse, error) {
 	// Not supported, do nothing
 
 	response := broker.UpdateInstanceResponse{}

pkg/minibroker/minibroker.go
@@ -345,7 +345,7 @@ func (c *Client) Provision(instanceID, serviceID, planID, namespace string, acce
 			OperationDescriptionKey: fmt.Sprintf("provisioning service instance %q", instanceID),
 		})
 		if err != nil {
-			return "", errors.Wrapf(err, "Failed to set operation key when provisioning instance %s", instanceID)
+			return "", errors.Wrapf(err, "Failed to set operation key when provisioning instance %q", instanceID)
 		}
 		go func() {
 			err = c.provisionSynchronously(instanceID, namespace, serviceID, planID, chartName, chartVersion, provisionParams)
@@ -385,13 +385,15 @@ func (c *Client) provisionSynchronously(instanceID, namespace, serviceID, planID
 		return err
 	}
 
+	chartURL := chartDef.URLs[0]
+
 	tc, close, err := c.connectTiller()
 	if err != nil {
 		return err
 	}
 	defer close()
 
-	chart, err := helm.LoadChart(chartDef)
+	chart, err := helm.LoadChart(chartURL)
 	if err != nil {
 		return err
 	}
@@ -659,11 +661,16 @@ func (c *Client) bindSynchronously(instanceID, serviceID, bindingID, releaseName
 
 // Unbind a previously-bound instance binding.
 func (c *Client) Unbind(instanceID, bindingID string) error {
-	// The only clean up we need to do is to remove the binding information
-	return c.updateConfigMap(instanceID, map[string]interface{}{
+	// The only clean up we need to do is to remove the binding information.
+	data := map[string]interface{}{
 		(BindingStateKeyPrefix + bindingID): nil,
 		(BindingKeyPrefix + bindingID):      nil,
-	})
+	}
+	if err := c.updateConfigMap(instanceID, data); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (c *Client) GetBinding(instanceID, bindingID string) (*osb.GetBindingResponse, error) {
@@ -755,7 +762,7 @@ func (c *Client) deprovisionSynchronously(instanceID, release string) error {
 }
 
 // LastOperationState returns the status of the last asynchronous operation.
-func (c *Client) LastOperationState(instanceID string, operationKey *osb.OperationKey) (*osb.LastOperationResponse, error) {
+func (c *Client) LastOperationState(instanceID string, operationKey osb.OperationKey) (*osb.LastOperationResponse, error) {
 	config, err := c.coreClient.CoreV1().ConfigMaps(c.namespace).Get(instanceID, metav1.GetOptions{})
 	if err != nil {
 		if apierrors.IsNotFound(err) {
@@ -768,12 +775,12 @@ func (c *Client) LastOperationState(instanceID string, operationKey *osb.Operati
 		return nil, err
 	}
 
-	if operationKey != nil && config.Data[OperationNameKey] != string(*operationKey) {
-		// Got unexpected operation key
+	if config.Data[OperationNameKey] != string(operationKey) {
+		// Got unexpected operation key.
 		return nil, osb.HTTPStatusCodeError{
 			StatusCode:   http.StatusBadRequest,
-			ErrorMessage: &[]string{ConcurrencyErrorMessage}[0],
-			Description:  &[]string{ConcurrencyErrorDescription}[0],
+			ErrorMessage: strPtr(ConcurrencyErrorMessage),
+			Description:  strPtr(ConcurrencyErrorDescription),
 		}
 	}
 
@@ -792,7 +799,7 @@ func strPtr(value string) *string {
 	return &value
 }
 
-func (c *Client) LastBindingOperationState(instanceID, bindingID string, operationKey *osb.OperationKey) (*osb.LastOperationResponse, error) {
+func (c *Client) LastBindingOperationState(instanceID, bindingID string) (*osb.LastOperationResponse, error) {
 	config, err := c.getConfigMap(instanceID)
 	if err != nil {
 		if apierrors.IsNotFound(err) {

pkg/helm/helm.go
@@ -123,9 +123,7 @@ func (c *Client) GetChart(name, version string) (*repo.ChartVersion, error) {
 	return nil, fmt.Errorf("version not found: %s @ %s", name, version)
 }
 
-func LoadChart(chart *repo.ChartVersion) (*chart.Chart, error) {
-	chartURL := chart.URLs[0]
-
+func LoadChart(chartURL string) (*chart.Chart, error) {
 	glog.Infof("downloading chart from %s", chartURL)
 	resp, err := http.Get(chartURL)
 	if err != nil {
@@ -156,9 +154,14 @@ func LoadChart(chart *repo.ChartVersion) (*chart.Chart, error) {
 
 	glog.Infof("copying chart to %s", fullChartPath)
 	if _, err := io.Copy(fd, resp.Body); err != nil {
-		return nil, errors.Wrapf(err, "failed to copy chart contents to %s", fullChartPath)
+		return nil, errors.Wrapf(err, "failed to download chart contents to %s", fullChartPath)
 	}
 
 	glog.Infof("loading chart from %s on disk", fullChartPath)
-	return chartutil.Load(fullChartPath)
+	chart, err := chartutil.Load(fullChartPath)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to load chart from disk")
+	}
+
+	return chart, nil
 }

commit 091f2df6d9cf9422b908d6020b1298275e2f01b5

fix: various broken pieces and improvements (#56)
* fix: use imagePullPolicy "Never" for devel

Signed-off-by: Thulio Ferraz Assis <thulio.assis@suse.com>

* fix: no need for -tags netgo when CGO_ENABLED=0

Signed-off-by: Thulio Ferraz Assis <thulio.assis@suse.com>

* fix: use sleep 1 on wait scripts to avoid spam

Signed-off-by: Thulio Ferraz Assis <thulio.assis@suse.com>

* fix: missing namespace creation for minibroker

Signed-off-by: Thulio Ferraz Assis <thulio.assis@suse.com>

* fix: reorg helm flags for deploy target

Signed-off-by: Thulio Ferraz Assis <thulio.assis@suse.com>

* fix: helm 3 syntax for wordpress targets

Signed-off-by: Thulio Ferraz Assis <thulio.assis@suse.com>

* fix: wait script for cluster creation

Signed-off-by: Thulio Ferraz Assis <thulio.assis@suse.com>

* refactor: explicit Dockerfile file copying

Signed-off-by: Thulio Ferraz Assis <thulio.assis@suse.com>

* fix: use multiple --set

Signed-off-by: Thulio Ferraz Assis <thulio.assis@suse.com>

* fix: create namespace when it doesn't exist

Signed-off-by: Thulio Ferraz Assis <thulio.assis@suse.com>

* fix: build image in minikube before deployment

Signed-off-by: Thulio Ferraz Assis <thulio.assis@suse.com>

* fix: bug introduced by #54

https://github.com/kubernetes-sigs/minibroker/pull/54/commits/3553969776700d7a8a07a81541f4be507403e9f6

Signed-off-by: Thulio Ferraz Assis <thulio.assis@suse.com>

* fix: missing license

Signed-off-by: Thulio Ferraz Assis <thulio.assis@suse.com>

Makefile
@@ -19,7 +19,7 @@ OUTPUT_DIR ?= output
 REGISTRY ?= quay.io/kubernetes-service-catalog/
 IMAGE ?= $(REGISTRY)minibroker
 TAG ?= canary
-IMAGE_PULL_POLICY ?= Always
+IMAGE_PULL_POLICY ?= Never
 
 lint: lint-go-vet lint-go-mod lint-modified-files
 
@@ -37,13 +37,16 @@ build:
 
 build-linux:
 	GOOS=linux GOARCH=amd64 CGO_ENABLED=0 \
-		go build -ldflags="-s -w" -o $(OUTPUT_DIR)/$(BINARY)-linux -tags netgo $(PKG)
+		go build -ldflags="-s -w" -o $(OUTPUT_DIR)/$(BINARY)-linux $(PKG)
 
 build-image:
 	docker build -t minibroker-build ./build/build-image
 
 image:
-	docker build --tag "$(IMAGE):$(TAG)" --file image/Dockerfile .
+	BUILD_IN_MINIKUBE=0 IMAGE="$(IMAGE)" TAG="$(TAG)" ./build/image.sh
+
+image-in-minikube:
+	BUILD_IN_MINIKUBE=1 IMAGE="$(IMAGE)" TAG="$(TAG)" ./build/image.sh
 
 clean:
 	-rm -rf $(OUTPUT_DIR)
@@ -64,23 +67,25 @@ test: test-unit test-mariadb test-mysqldb test-postgresql test-mongodb test-word
 test-wordpress: setup-wordpress teardown-wordpress
 
 setup-wordpress:
-	helm install --name minipress charts/wordpress --wait
+	kubectl create namespace minibroker-test-wordpress
+	helm install minipress charts/wordpress --namespace minibroker-test-wordpress --wait
 
 teardown-wordpress:
-	helm delete --purge minipress
+	helm delete minipress --namespace minibroker-test-wordpress
+	kubectl delete namespace minibroker-test-wordpress
 
 test-mysqldb: setup-mysqldb teardown-mysqldb
 
 setup-mysqldb:
-	until svcat get broker minibroker | grep -w -m 1 Ready; do : ; done
+	until svcat get broker minibroker | grep -w -m 1 Ready; do sleep 1 ; done
 
 	svcat provision mysqldb --class mysql --plan 5-7-14 --namespace minibroker \
 		-p mysqlDatabase=mydb -p mysqlUser=admin
-	until svcat get instance mysqldb -n minibroker | grep -w -m 1 Ready; do : ; done
+	until svcat get instance mysqldb -n minibroker | grep -w -m 1 Ready; do sleep 1 ; done
 	svcat get instance mysqldb -n minibroker
 
 	svcat bind mysqldb -n minibroker
-	until svcat get binding mysqldb -n minibroker | grep -w -m 1 Ready; do : ; done
+	until svcat get binding mysqldb -n minibroker | grep -w -m 1 Ready; do sleep 1 ; done
 	svcat describe binding mysqldb -n minibroker
 
 teardown-mysqldb:
@@ -90,15 +95,15 @@ teardown-mysqldb:
 test-mariadb: setup-mariadb teardown-mariadb
 
 setup-mariadb:
-	until svcat get broker minibroker | grep -w -m 1 Ready; do : ; done
+	until svcat get broker minibroker | grep -w -m 1 Ready; do sleep 1 ; done
 
 	svcat provision mariadb --class mariadb --plan 10-1-32 --namespace minibroker \
 		--params-json '{"db": {"name": "mydb", "user": "admin"}}'
-	until svcat get instance mariadb -n minibroker | grep -w -m 1 Ready; do : ; done
+	until svcat get instance mariadb -n minibroker | grep -w -m 1 Ready; do sleep 1 ; done
 	svcat get instance mariadb -n minibroker
 
 	svcat bind mariadb -n minibroker
-	until svcat get binding mariadb -n minibroker | grep -w -m 1 Ready; do : ; done
+	until svcat get binding mariadb -n minibroker | grep -w -m 1 Ready; do sleep 1 ; done
 	svcat describe binding mariadb -n minibroker
 
 teardown-mariadb:
@@ -108,15 +113,15 @@ teardown-mariadb:
 test-postgresql: setup-postgresql teardown-postgresql
 
 setup-postgresql:
-	until svcat get broker minibroker | grep -w -m 1 Ready; do : ; done
+	until svcat get broker minibroker | grep -w -m 1 Ready; do sleep 1 ; done
 
 	svcat provision postgresql --class postgresql --plan 11-0-0 --namespace minibroker \
 		-p postgresDatabase=mydb -p postgresUser=admin
-	until svcat get instance postgresql -n minibroker | grep -w -m 1 Ready; do : ; done
+	until svcat get instance postgresql -n minibroker | grep -w -m 1 Ready; do sleep 1 ; done
 	svcat get instance postgresql -n minibroker
 
 	svcat bind postgresql -n minibroker
-	until svcat get binding postgresql -n minibroker | grep -w -m 1 Ready; do : ; done
+	until svcat get binding postgresql -n minibroker | grep -w -m 1 Ready; do sleep 1 ; done
 	svcat describe binding postgresql -n minibroker
 
 teardown-postgresql:
@@ -126,15 +131,15 @@ teardown-postgresql:
 test-mongodb: setup-mongodb teardown-mongodb
 
 setup-mongodb:
-	until svcat get broker minibroker | grep -w -m 1 Ready; do : ; done
+	until svcat get broker minibroker | grep -w -m 1 Ready; do sleep 1 ; done
 
 	svcat provision mongodb --class mongodb --plan 3-7-1 --namespace minibroker \
 		-p mongodbDatabase=mydb -p postgresUsername=admin
-	until svcat get instance mongodb -n minibroker | grep -w -m 1 Ready; do : ; done
+	until svcat get instance mongodb -n minibroker | grep -w -m 1 Ready; do sleep 1 ; done
 	svcat get instance mongodb -n minibroker
 
 	svcat bind mongodb -n minibroker
-	until svcat get binding mongodb -n minibroker | grep -w -m 1 Ready; do : ; done
+	until svcat get binding mongodb -n minibroker | grep -w -m 1 Ready; do sleep 1 ; done
 	svcat describe binding mongodb -n minibroker
 
 teardown-mongodb:
@@ -147,11 +152,18 @@ log:
 create-cluster:
 	./hack/create-cluster.sh
 
-deploy:
-	until svcat version | grep -m 1 'Server Version: v' ; do : ; done
-	helm upgrade --install minibroker --namespace minibroker \
-	--recreate-pods --force charts/minibroker \
-	--set image="$(IMAGE):$(TAG)",imagePullPolicy="$(IMAGE_PULL_POLICY)",deploymentStrategy="Recreate"
+deploy: image-in-minikube
+	until svcat version | grep -m 1 'Server Version: v' ; do sleep 1 ; done
+	if ! kubectl get namespace minibroker 1> /dev/null 2> /dev/null; then kubectl create namespace minibroker; fi
+	helm upgrade minibroker \
+		--install \
+		--force \
+		--recreate-pods \
+		--namespace minibroker \
+		--set "image=$(IMAGE):$(TAG)" \
+		--set "imagePullPolicy=$(IMAGE_PULL_POLICY)" \
+		--set "deploymentStrategy=Recreate" \
+		charts/minibroker
 
 release: release-images release-charts
 

build/image.sh
@@ -1,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Copyright 2020 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit -o nounset -o pipefail
+
+if [[ "${BUILD_IN_MINIKUBE}" == "1" ]]; then
+  # shellcheck disable=SC2046
+  eval $(minikube -p minikube docker-env)
+fi
+
+docker build --tag "${IMAGE}:${TAG}" --file image/Dockerfile .

hack/create-cluster.sh
@@ -43,7 +43,7 @@ helm install "${catalog_release}" \
   "${catalog_repository}/catalog"
 
 set +o xtrace
-while [[ "$(kubectl get pods --namespace "${catalog_namespace}" --selector "release=${catalog_release}" --output=go-template='{{.items||len}}')" == 0 ]]; do
+while [[ "$(kubectl get pods --namespace "${catalog_namespace}" --selector "release=${catalog_release}" --output=go-template='{{.items | len}}')" == 0 ]]; do
   sleep 1;
 done
 set -o xtrace

image/Dockerfile
@@ -26,7 +26,9 @@ WORKDIR /build/minibroker
 COPY go.mod go.sum ./
 RUN go mod download
 
-COPY ./ ./
+COPY cmd/ ./cmd/
+COPY pkg/ ./pkg/
+COPY Makefile ./
 RUN make build-linux
 
 # --------------------------------------------------------------------------------------------------

pkg/broker/broker.go
@@ -18,7 +18,6 @@ package broker
 
 import (
 	"errors"
-	"fmt"
 	"sync"
 
 	"github.com/golang/glog"
@@ -138,10 +137,7 @@ func (b *Broker) LastOperation(request *osb.LastOperationRequest, _ *broker.Requ
 	b.Lock()
 	defer b.Unlock()
 
-	if request.OperationKey == nil {
-		return nil, fmt.Errorf("failed to get last operation for instance %q: operation key cannot be nil", request.InstanceID)
-	}
-	response, err := b.Client.LastOperationState(request.InstanceID, *request.OperationKey)
+	response, err := b.Client.LastOperationState(request.InstanceID, request.OperationKey)
 	if err != nil {
 		glog.Errorln(err)
 		return nil, err

pkg/minibroker/minibroker.go
@@ -762,7 +762,7 @@ func (c *Client) deprovisionSynchronously(instanceID, release string) error {
 }
 
 // LastOperationState returns the status of the last asynchronous operation.
-func (c *Client) LastOperationState(instanceID string, operationKey osb.OperationKey) (*osb.LastOperationResponse, error) {
+func (c *Client) LastOperationState(instanceID string, operationKey *osb.OperationKey) (*osb.LastOperationResponse, error) {
 	config, err := c.coreClient.CoreV1().ConfigMaps(c.namespace).Get(instanceID, metav1.GetOptions{})
 	if err != nil {
 		if apierrors.IsNotFound(err) {
@@ -775,7 +775,7 @@ func (c *Client) LastOperationState(instanceID string, operationKey osb.Operatio
 		return nil, err
 	}
 
-	if config.Data[OperationNameKey] != string(operationKey) {
+	if operationKey != nil && config.Data[OperationNameKey] != string(*operationKey) {
 		// Got unexpected operation key.
 		return nil, osb.HTTPStatusCodeError{
 			StatusCode:   http.StatusBadRequest,