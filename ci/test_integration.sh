6eb3d4fc0b00cebaa7fcc06746550ecde86e8eb8
Thulio Ferraz Assis <thulio.assis@suse.com>
2020-05-29 11:24:40 -0500

    fix: environment variables
    Signed-off-by: Thulio Ferraz Assis <thulio.assis@suse.com>

--- ci/test_integration.sh
+++ ci/test_integration.sh
@@ -17,6 +17,5 @@
 set -o errexit -o nounset -o pipefail
 
 VM_DRIVER=none sudo timeout 3m make create-cluster
-sudo chown -R "$(whoami):" "${HOME}/.minikube/"
 timeout 5m make deploy
 timeout 5m make test-integration
