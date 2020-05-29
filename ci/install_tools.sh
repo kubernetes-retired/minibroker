8202f5dcdf9ee2e35880adc7d53bbc52b41a12c2
Thulio Ferraz Assis <thulio.assis@suse.com>
2020-05-29 11:44:42 -0500

    fix: checksum comparison
    Signed-off-by: Thulio Ferraz Assis <thulio.assis@suse.com>

--- ci/install_tools.sh
+++ ci/install_tools.sh
@@ -21,7 +21,7 @@ version="v1.18.1"
 sha256="f5144823e6d8a0b78611a8d12e7a25202126d079c3a232b18f37e61e872ff563"
 asset_path="${HOME}/assets/kubectl"
 asset_url="https://storage.googleapis.com/kubernetes-release/release/${version}/bin/linux/amd64/kubectl"
-if [ ! -f "${asset_path}" ] || [[ "$(sha256sum "${asset_path}")" != "${sha256}" ]]; then
+if [ ! -f "${asset_path}" ] || [[ "$(sha256sum "${asset_path}" | awk '{ print $1 }')" != "${sha256}" ]]; then
   curl -Lo "${asset_path}" "${asset_url}"
   chmod +x "${asset_path}"
 fi
@@ -32,7 +32,7 @@ version="v1.10.1"
 sha256="acc67ea2ff1ca261269a702a6d998367f65c86d9024c20bbf5ac3922bfca1aaa"
 asset_path="${HOME}/assets/minikube"
 asset_url="https://storage.googleapis.com/minikube/releases/${version}/minikube-linux-amd64"
-if [ ! -f "${asset_path}" ] || [[ "$(sha256sum "${asset_path}")" != "${sha256}" ]]; then
+if [ ! -f "${asset_path}" ] || [[ "$(sha256sum "${asset_path}" | awk '{ print $1 }')" != "${sha256}" ]]; then
   curl -Lo "${asset_path}" "${asset_url}"
   chmod +x "${asset_path}"
 fi
@@ -43,7 +43,7 @@ version="v3.2.1"
 sha256="018f9908cb950701a5d59e757653a790c66d8eda288625dbb185354ca6f41f6b"
 asset_path="${HOME}/assets/helm.tar.gz"
 asset_url="https://get.helm.sh/helm-${version}-linux-amd64.tar.gz"
-if [ ! -f "${asset_path}" ] || [[ "$(sha256sum "${asset_path}")" != "${sha256}" ]]; then
+if [ ! -f "${asset_path}" ] || [[ "$(sha256sum "${asset_path}" | awk '{ print $1 }')" != "${sha256}" ]]; then
   curl -Lo "${asset_path}" "${asset_url}"
 fi
 sudo tar zxf "${asset_path}" --strip-components=1 --directory /usr/local/bin/ linux-amd64/helm
@@ -53,7 +53,7 @@ version="v0.3.0"
 sha256="84ec798e8837982dfe13e5a02bf83e801af2461323ab2c441787d7d9f7bad60a"
 asset_path="${HOME}/assets/svcat"
 asset_url="https://download.svcat.sh/cli/${version}/linux/amd64/svcat"
-if [ ! -f "${asset_path}" ] || [[ "$(sha256sum "${asset_path}")" != "${sha256}" ]]; then
+if [ ! -f "${asset_path}" ] || [[ "$(sha256sum "${asset_path}" | awk '{ print $1 }')" != "${sha256}" ]]; then
   curl -Lo "${asset_path}" "${asset_url}"
   chmod +x "${asset_path}"
 fi
