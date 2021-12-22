4c2f59ce92f17a928f75058e561ec5b2c0e7e58e
Thulio Ferraz Assis <3149049+f0rmiga@users.noreply.github.com>
2021-12-21 22:20:51 -0800

    fix: create binaries cache dir
    Signed-off-by: Thulio Ferraz Assis <3149049+f0rmiga@users.noreply.github.com>

--- ci/install/integration.sh
+++ ci/install/integration.sh
@@ -16,10 +16,13 @@
 
 set -o errexit -o nounset -o pipefail
 
+readonly cache_dir="${HOME}/.cache/binaries"
+mkdir -p "${cache_dir}"
+
 # Install kubectl.
 version="v1.18.1"
 sha256="f5144823e6d8a0b78611a8d12e7a25202126d079c3a232b18f37e61e872ff563"
-asset_path="${HOME}/.cache/binaries/kubectl"
+asset_path="${cache_dir}/kubectl"
 asset_url="https://dl.k8s.io/release/${version}/bin/linux/amd64/kubectl"
 if [ ! -f "${asset_path}" ] || [[ "$(sha256sum "${asset_path}" | awk '{ print $1 }')" != "${sha256}" ]]; then
   curl -Lo "${asset_path}" "${asset_url}"
@@ -30,7 +33,7 @@ sudo cp "${asset_path}" /usr/local/bin/kubectl
 # Install minikube.
 version="v1.10.1"
 sha256="acc67ea2ff1ca261269a702a6d998367f65c86d9024c20bbf5ac3922bfca1aaa"
-asset_path="${HOME}/.cache/binaries/minikube"
+asset_path="${cache_dir}/minikube"
 asset_url="https://storage.googleapis.com/minikube/releases/${version}/minikube-linux-amd64"
 if [ ! -f "${asset_path}" ] || [[ "$(sha256sum "${asset_path}" | awk '{ print $1 }')" != "${sha256}" ]]; then
   curl -Lo "${asset_path}" "${asset_url}"
@@ -41,7 +44,7 @@ sudo cp "${asset_path}" /usr/local/bin/minikube
 # Install helm.
 version="v3.2.1"
 sha256="018f9908cb950701a5d59e757653a790c66d8eda288625dbb185354ca6f41f6b"
-asset_path="${HOME}/.cache/binaries/helm.tar.gz"
+asset_path="${cache_dir}/helm.tar.gz"
 asset_url="https://get.helm.sh/helm-${version}-linux-amd64.tar.gz"
 if [ ! -f "${asset_path}" ] || [[ "$(sha256sum "${asset_path}" | awk '{ print $1 }')" != "${sha256}" ]]; then
   curl -Lo "${asset_path}" "${asset_url}"
@@ -51,7 +54,7 @@ sudo tar zxf "${asset_path}" --strip-components=1 --directory /usr/local/bin/ li
 # Install svcat.
 version="v0.3.0"
 sha256="84ec798e8837982dfe13e5a02bf83e801af2461323ab2c441787d7d9f7bad60a"
-asset_path="${HOME}/.cache/binaries/svcat"
+asset_path="${cache_dir}/svcat"
 asset_url="https://download.svcat.sh/cli/${version}/linux/amd64/svcat"
 if [ ! -f "${asset_path}" ] || [[ "$(sha256sum "${asset_path}" | awk '{ print $1 }')" != "${sha256}" ]]; then
   curl -Lo "${asset_path}" "${asset_url}"
